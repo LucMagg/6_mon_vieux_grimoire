5dfa7cce57012817751ad3d127d39b10da0443b7
LucMagg <luc.maggiotto@gmail.com>
2024-05-05 14:17:31 +0200

    feat js : ajout du controller getOneBook

--- controllers/book.js
+++ controllers/book.js
@@ -1,13 +1,15 @@
 const Book = require('../models/book')
 
-exports.getAllBooks = (req, res, next) => {
+exports.getAllBooks = (req, res) => {
     Book.find()
     .then(books => res.status(200).json(books))
     .catch(error => res.status(400).json({ error }))
 }
 
-exports.getOneBook = (req, res, next) => {
-
+exports.getOneBook = (req, res) => {
+    Book.findOne( {_id: req.params._id })
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(404).json({ error }))
 }
 
 exports.getBestRatedBooks = (req, res, next) => {
