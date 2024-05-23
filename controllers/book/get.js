56df52574e23c8381bc57edc59e6413405c94f4b
LucMagg <luc.maggiotto@gmail.com>
2024-05-23 13:37:41 +0200

    refactor js : modif des controllers book/get
    englobage dans un try/catch + ajout du 404 livre non trouvé

--- controllers/book/get.js
+++ controllers/book/get.js
@@ -1,28 +1,37 @@
 const Book = require('../../models/book')
 
 
-const getAllBooks = (req, res, next) => {
-    Book.find()
-    .then(books => {
-            res.status(200).json(books)
-    })
-    .catch(error => res.status(400).json({ error }))
+const getAllBooks = async (req, res, next) => {
+    try {
+        const books = await Book.find()
+        res.status(200).json(books)
+    } catch (error) {
+        res.status(400).json({ error })
+    } 
 }
 
 
 const getOneBook = (req, res, next) => {
-    Book.findOne( {_id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }))
+    try {
+        const book = Book.findOne( {_id: req.params.id })
+        if (book) {
+            res.status(200).json(book)
+        } else {
+            res.status(404).json( {message : `Aucun livre référencé avec l'id ${req.params.id}`})
+        }
+    } catch (error) {
+        res.status(404).json({ error })
+    }
 }
 
 
 const getBestRatedBooks = (req, res, next) => {
-    Book.find()
-    .then(books => {
+    try {
+        const books = Book.find()
         res.status(200).json(bestRatedBooks(books))
-    })
-    .catch(error => res.status(400).json({ error }))
+    } catch(error) {
+        res.status(400).json({ error })
+    }
 }
 
 
